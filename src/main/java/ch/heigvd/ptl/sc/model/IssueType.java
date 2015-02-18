0b39cfa8fac1e3e75547a5cd88bd8ec56895a4f7
Laurent Lienher <laurent.lienher@gmail.com>
2015-02-18 15:30:33 +0100

    issueType completed

--- src/main/java/ch/heigvd/ptl/sc/model/IssueType.java
+++ src/main/java/ch/heigvd/ptl/sc/model/IssueType.java
@@ -8,6 +8,27 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class IssueType {
     @Id
     private String id;
+    private String name;
+    private String description;
     
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getDescription(){
+        return description;
+    }
+    
+    public void setDescription(String description){
+        this.description = description;
+    }
+    
+    public String getId(){
+        return id;
+    }
     
 }
