4c8adec76c07f536871785dd3e4dfea395c6fec9
Laurent Lienher <laurent.lienher@gmail.com>
2015-02-18 15:14:20 +0100

    IssueType Model created

---
+++ src/main/java/ch/heigvd/ptl/sc/model/IssueType.java
@@ -0,0 +1,13 @@
+package ch.heigvd.ptl.sc.model;
+
+import java.util.List;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class IssueType {
+    @Id
+    private String id;
+    
+    
+}
